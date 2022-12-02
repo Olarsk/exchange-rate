2e437162244221b95708ba27ef7311a12aabdef1
Ahmad Ibrahim <97315843+Olarsk@users.noreply.github.com>
2022-12-02 17:06:59 +0100

    Initial commit

---
+++ naira_rates_tweets.py
@@ -0,0 +1,19 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+query = "-crypto (from:naira_rates) until:2022-12-01 since:2022-01-01"
+tweets = []
+limit = 1000
+
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+
+    # print(vars(tweet))
+    # break
+    if len(tweets) == limit:
+        break
+    else:
+        tweets.append([tweet.date, tweet.user.username, tweet.content])
+
+df = pd.DataFrame(tweets, columns=['Date', 'Username', 'Tweet'])
+df.to_csv('nr_blackmarket_raw.csv')
+# print(df)
